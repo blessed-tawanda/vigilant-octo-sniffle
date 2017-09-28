006166372f7ab44c31023abe53df908239755bea
blessed-tawanda <blessedmahuni@gmail.com>
2017-09-28 21:44:13 +0200

    added the last function left now left with developing the UI

--- index.js
+++ index.js
@@ -148,6 +148,70 @@ function hexadecimalToBinary(num){
     var decimalEquivalent = hexadecimalToDecimal(num);
     return decimalToBinary(decimalEquivalent);
 }
+function decimalToOctal(num){
+    var remainders = [];
+    var remainder;
+    var qotient;
+    qotient = num;
+    while(qotient>0)
+    {
+        remainder = qotient%8;
+        qotient = Math.floor(qotient/8);
+        remainders.push(remainder);
+    }
+    remainders = remainders.reverse();
+    remainders = remainders.join('');
+    return remainders;
+}
+// maybe i should create a to decimal function that is passed the number base and number to be converted :) food for thought
+function octalToDecimal(num){
+    //just copied sam code as with binary to decimal just change the 8 in the while loop
+    var binArr; 
+    var bitSize;
+    var nums = [];
+    var bit = [];
+    var temp;
+    var decimal;
+    binArr = num.toString();
+    binArr = binArr.split('');
+    bitSize = binArr.length-1;
+ 
+    while(bitSize>=0)
+    {
+        temp = Math.pow(8,bitSize); //<= this is the only the line i changed from the binary to decimal function
+        bit.push(temp);
+        bitSize--;
+    }
+    temp = 0;
+    for(var i = 0;i<bit.length;i++)
+    {
+        temp = binArr[i]*bit[i];
+        nums.push(temp);
+    }
+    decimal = 0;
+    for(var j = 0;j<nums.length;j++)
+    {
+        temp=Number(nums[j]);
+        decimal = decimal + temp;
+    }
+    return decimal;
 
+}
+function binaryToOctal(num){
+    var decimalEquivalent = binaryToDecimal(num);
+    return decimalToOctal(decimalEquivalent);
+}
+function octalToBinary(num){
+    var decimalEquivalent = octalToDecimal(num);
+    return decimalToBinary(decimalEquivalent);
+}
+function hexadecimalToOctal(num){
+    var decimalEquivalent = hexadecimalToDecimal(num);
+    return decimalToOctal(decimalEquivalent);
+}
+function octalToHexadecimal(num){
+    var decimalEquivalent = octalToDecimal(num);
+    return decimalToHexadecimal(decimalEquivalent);
+}
 
-console.log(hexadecimalToBinary('123'));
+console.log(octalToHexadecimal('63'));
